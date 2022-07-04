929dcb9e3cc197a24998374aedcced10b3ab6ab7
pavithramuddu93 <100592978+pavithramuddu93@users.noreply.github.com>
2022-07-04 22:20:13 +0530

    Update Queue.java

--- src/blz/Queue.java
+++ src/blz/Queue.java
@@ -1,5 +1,18 @@
 package blz;
 
 public class Queue {
+  LinkedList list = new LinkedList();
+
+	public void enQueue(Object data) {
+		list.addLast(data);
+	}
+
+	public void deQueue() {
+		list.deleteFirst();
+	}
+
+	public void display() {
+		list.display();
+	}
 
 }
