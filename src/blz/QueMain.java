a9e8bd6e746a6d7ed301fda1926c6b25b87dc48f
pavithramuddu93 <100592978+pavithramuddu93@users.noreply.github.com>
2022-07-04 22:33:39 +0530

    Update QueMain.java

--- src/blz/QueMain.java
+++ src/blz/QueMain.java
@@ -2,4 +2,16 @@ package blz;
 
 public class QueMain {
 
+public static void main(String[] args) {
+
+		Queue queue = new Queue();
+		System.out.println("Queue Operation - EnQueue");
+		queue.enQueue(56);
+		queue.display();
+		queue.enQueue(30);
+		queue.display();
+		queue.enQueue(70);
+		queue.display();
+	}
+
 }
