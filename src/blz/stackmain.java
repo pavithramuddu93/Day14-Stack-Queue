a63e256c06f8dd7c8c88965a87e1dd83bffd7cc6
pavithramuddu93 <pavithramuddu93>
2022-07-04 22:14:54 +0530

    UC2 added

--- src/blz/stackmain.java
+++ src/blz/stackmain.java
@@ -2,4 +2,26 @@ package blz;
 
 public class stackmain {
 
+
+	public static void main(String[] args) {
+
+		Stack stack = new Stack();
+		System.out.println("Stack Operation Adding");
+		stack.push(70);
+		stack.display();
+		stack.push(30);
+		stack.display();
+		stack.push(56);
+		stack.display();
+		System.out.println("Stack Operation Deleting");
+		stack.display();
+		stack.pop();
+		stack.display();
+		stack.pop();
+		stack.display();
+		stack.pop();
+		stack.display();
+
+
+	}
 }
